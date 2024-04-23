e2c787251ec8115bc6a1adbac9e4a7e3f236f4fb
anhuynh75 <ahuynh750@gmail.com>
2024-04-23 09:22:10 -0500

    Commit to git with files worked on over spring.

---
+++ js/faq.js
@@ -0,0 +1,9 @@
+console.log("Hello World!");
+
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq) => {
+    faq.addEventListener("click", () => {
+        faq.classList.toggle("active");
+    })
+})
